fc79388219bf0c7fd2cb2bd59a93f6b3c9f83dcd
radames <radames@x50>
2019-09-11 17:20:22 -0300

    gerador

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -13,12 +13,22 @@ public class Main {
         //entrada
         double a = entrada.lerNumeroDouble("Digite um valor para A");
         double b = entrada.lerNumeroDouble("Digite um valor para B");
-
+        double c = entrada.lerNumeroDouble("Digite um valor para C");
         //processamento
-        Double soma = processamento.getSoma(a, b);
+        String resp = processamento.delta(a, b,c);
+        if (resp.equals("Ok")) {
+             Double x1 = processamento.getX1(a, b);
+             Double x2 = processamento.getX2(a, b);
+             saida.imprimirNumeroDouble("x1", x1);
+             saida.imprimirNumeroDouble("x2", x2);
+        } else{
+            saida.imprimirRotuloEString("Resp", resp);
+        }
+       
+       
 
         //saída
-        saida.imprimirNumeroDouble("Resultado da soma", soma);
+        
 
     }
 
--- src/Main/Processamento.java
+++ src/Main/Processamento.java
@@ -3,8 +3,26 @@ package Main;
 //@author Radames J Halmeman  - rjhalmeman@gmail.com
 class Processamento {
 
-    public Double getSoma(double a, double b) {
-        double resultado = a + b;
-        return resultado;
+    double delta = 0;
+
+    public String delta(double a, double b, double c) {
+        delta = b * b - 4 * a * c;
+        if (delta < 0) {
+            return "impossivel";
+        } else if (a == 0) {
+            return "nao existe divisao por 0";
+        }
+        return "Ok";
     }
+
+    public Double getX1(double a, double b) {
+        double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        return x1;
+    }
+    public Double getX2(double a, double b) {
+        double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        return x2;
+    }
+
+   
 }
