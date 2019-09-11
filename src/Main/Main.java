1aefa5efa091d1c4dc985fc39fb25fd2c586b07d
karen <karen@LAPTOP-AI6UI19U>
2019-09-11 19:09:44 -0300

    programinea de imc feito pela karen

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,21 +4,27 @@ package Main;
  * @author Radames J Halmeman - rjhalmeman@gmail.com
  */
 public class Main {
-
+double classificacao=0;
     public static void main(String[] args) {
         Entrada entrada = new Entrada();
         Processamento processamento = new Processamento();
         Saida saida = new Saida();
-        
+
         //entrada
-        double a = entrada.lerNumeroDouble("Digite um valor para A");
-        double b = entrada.lerNumeroDouble("Digite um valor para B");
-
+        double peso= entrada.lerNumeroDouble("Digite seu peso: ");
+        double altura= entrada.lerNumeroDouble("Digite sua altura: ");
         //processamento
-        Double soma = processamento.getSoma(a, b);
-
+         if(altura>0){
+        double imc=processamento.getImc(peso,altura);
+        double classificacao=processamento.Getclassifica(imc);
+        saida.imprimirNumeroDouble("imc", classificacao);
+        }
+         else{
+             System.out.println("nao existe divisao por 0");
+         }
         //saída
-        saida.imprimirNumeroDouble("Resultado da soma", soma);
+        
+       
 
     }
 
--- src/Main/Processamento.java
+++ src/Main/Processamento.java
@@ -1,10 +1,49 @@
 package Main;
 
-//@author Radames J Halmeman  - rjhalmeman@gmail.com
+//
 class Processamento {
 
-    public Double getSoma(double a, double b) {
-        double resultado = a + b;
-        return resultado;
+    double imc = 0;
+
+    public double getImc(double peso, double altura) {
+        imc = peso / (altura * altura);
+        return imc;
     }
+
+    public String verificaExistencia(double altura) {
+        if(altura>0){
+            return "ok";
+        } else{
+            return "nao existe divisao por zero";
+        }
+    }
+
+    public double Getclassifica(double imc) {
+        if (imc < 16.00) {
+            System.out.println("Baixo peso grau II");
+        }
+        if (imc >= 16.00 && imc <= 16.99) {
+            System.out.println("Baixo peso grau I");
+        }
+        if (imc >= 17.00 && imc <= 18.49) {
+            System.out.println("Baixo peso grau II");
+        }
+        if (imc >= 18.50 && imc <= 24.99) {
+            System.out.println("Peso ideal");
+        }
+        if (imc >= 25.00 && imc <= 29.99) {
+            System.out.println("Sobrepeso");
+        }
+        if (imc >= 30.00 && imc <= 34.99) {
+            System.out.println("Obesidade grau I");
+        }
+        if (imc >= 35.00 && imc <= 39.99) {
+            System.out.println("Obesidade grau II");
+        }
+        if (imc >= 40.00) {
+            System.out.println("Obesidade grau III");
+        }
+        return imc;
+    }
+
 }
